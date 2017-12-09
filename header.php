337cc2cc99feb64f03778b31940d5fd5a9af9277
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-09 23:15:38 +0100

    Nuevo script de cabecera

---
+++ header.php
@@ -0,0 +1,19 @@
+<HEADER id="cajaTitulo">
+    <FIGURE>
+        <IMG src="./img/Logotipo.png" alt="Mi Logo" title="Logotipo del sitio web" longdesc="http://www.miweb.es/index.html#descripcionweb" />
+        <FIGCAPTION>
+            <H1 id="titulo">Panel de Administración Hotel</H1>
+            <P id="descripcionweb">Adminstra desde este panel clientes y habitaciones</P>
+        </FIGCAPTION>
+    </FIGURE>
+</HEADER>
+
+<NAV id="cajaMenuPrincipal"><!--Menú principal bajo el título-->
+    <UL>
+        <LI><A href="./index.php" title="Inicio">Inicio</A></LI>
+        <LI><A href="./clientes.php" title="Clientes">Clientes</A></LI>
+        <LI><A href="./habitaciones.php" title="Habitaciones">Habitaciones</A></LI>
+        <LI><A href="./reservas.php" title="Reservas">Reservas</A></LI>
+        <LI><A href="./estadisticas.php" title="Estadísticas">Estadísticas</A></LI>
+    </UL>
+</NAV>
