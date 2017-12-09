efb877f0d85e5ecfa62472e48bf5e227145eb532
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-10 00:09:05 +0100

    Planteada consulta de todos los clientes

--- bd/consultas.php
+++ bd/consultas.php
@@ -3,11 +3,14 @@
 function consulta_clientes_todos()
 {
     conectar();
+    return $clientes = $pdo->query('SELECT * FROM clientes');
 }
 
 function consulta_clientes_con_reserva()
 {
-
+    // $clientes = consulta_clientes_todos();
+    // Filtrar resultados¿?
+    // desconectar();
 }
 
 function consulta_clientes_sin_reserva()
