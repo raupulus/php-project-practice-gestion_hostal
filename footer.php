617357a6b9dcb86b2759d22652892f8010adc069
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-09 23:58:59 +0100

    Etiquetas en minúsculas

--- footer.php
+++ footer.php
@@ -1,35 +1,37 @@
-<FOOTER>
-    <P>Este es el pie de página</P>
-    <DIV id="cajaEnlacesFooter">
-        <NAV>
-            <UL>
-                <LI><A href="#" title="Link 1">Enlace 1</A></LI>
-                <LI><A href="#" title="Link 2">Enlace 2</A></LI>
-                <LI><A href="#" title="Link 3">Enlace 3</A></LI>
-                <LI><A href="#" title="Link 4">Enlace 4</A></LI>
-            </UL>
-        </NAV>
+<footer>
+    <p>Este es el pie de página</p>
+    <div id="cajaEnlacesFooter">
+        <nav>
+            <ul>
+                <li><a href="#" title="Link 1">Enlace 1</a></li>
+                <li><a href="#" title="Link 2">Enlace 2</a></li>
+                <li><a href="#" title="Link 3">Enlace 3</a></li>
+                <li><a href="#" title="Link 4">Enlace 4</a></li>
+            </ul>
+        </nav>
 
-        <NAV>
-            <UL>
-                <LI><A href="#" title="Link 1">Enlace 1</A></LI>
-                <LI><A href="#" title="Link 2">Enlace 2</A></LI>
-                <LI><A href="#" title="Link 3">Enlace 3</A></LI>
-                <LI><A href="#" title="Link 4">Enlace 4</A></LI>
-            </UL>
-        </NAV>
+        <nav>
+            <ul>
+                <li><a href="#" title="Link 1">Enlace 1</a></li>
+                <li><a href="#" title="Link 2">Enlace 2</a></li>
+                <li><a href="#" title="Link 3">Enlace 3</a></li>
+                <li><a href="#" title="Link 4">Enlace 4</a></li>
+            </ul>
+        </nav>
 
-        <NAV>
-            <UL>
-                <LI><A href="#" title="Link 1">Enlace 1</A></LI>
-                <LI><A href="#" title="Link 2">Enlace 2</A></LI>
-                <LI><A href="#" title="Link 3">Enlace 3</A></LI>
-                <LI><A href="#" title="Link 4">Enlace 4</A></LI>
-            </UL>
-        </NAV>
-    </DIV>
-    <P>Sitio creado por <A href="mailto:tecnico@fryntiz.es" title="Enviar correo a Fryntiz">tecnico@fryntiz.es</A>, puedes visitar mi web <A href="http://www.fryntiz.es" title="Web del programador Fryntiz" target="_blank">www.fryntiz.es</A></P>
-    <P><SMALL>Licencia GPLv3 comparte y reconoce al autor | 2016 Creado y diseñado originalmente por Raúl Caro Pastorino</SMALL></P>
+        <nav>
+            <ul>
+                <li><a href="#" title="Link 1">Enlace 1</a></li>
+                <li><a href="#" title="Link 2">Enlace 2</a></li>
+                <li><a href="#" title="Link 3">Enlace 3</a></li>
+                <li><a href="#" title="Link 4">Enlace 4</a></li>
+            </ul>
+        </nav>
+    </div>
 
-    <A id="subir" href="#titulo" rel="nofollow" title="Subir"><DIV>Subir</DIV></A>
-</FOOTER>
+    <p>Sitio creado por <a href="mailto:tecnico@fryntiz.es" title="Enviar correo a Fryntiz">tecnico@fryntiz.es</a>, puedes visitar mi web <a href="http://www.fryntiz.es" title="Web del programador Fryntiz" target="_blank">www.fryntiz.es</a></p>
+
+    <p><small>Licencia GPLv3 comparte y reconoce al autor | 2016 Creado y diseñado originalmente por Raúl Caro Pastorino</small></p>
+
+    <a id="subir" href="#titulo" rel="nofollow" title="Subir"><div>Subir</div></a>
+</footer>
