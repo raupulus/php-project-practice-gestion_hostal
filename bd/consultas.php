39f5bd552c8ded0bbd9738abd26c1cabe1a758f0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-09 23:31:10 +0100

    Planteadas funciones de consultas para clientes

---
+++ bd/consultas.php
@@ -0,0 +1,26 @@
+<?php
+
+function consulta_clientes_todos()
+{
+    conectar();
+}
+
+function consulta_clientes_con_reserva()
+{
+
+}
+
+function consulta_clientes_sin_reserva()
+{
+
+}
+
+function consulta_clientes_nunca_reservaron()
+{
+
+}
+
+function consulta_clientes_fecha_ultima_reserva()
+{
+
+}
