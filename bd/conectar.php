8cbb24fd9f054cfa14d179c14c62a08e74a630c5
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-09 23:05:44 +0100

    Adaptada función conectar a objeto PDO

--- bd/conectar.php
+++ bd/conectar.php
@@ -1,31 +1,44 @@
 <?php
-//Datos de Conexión
-$host="127.0.0.1";
-$port="5432";
-$user="hostal";
-$pass="123456";
-$dbname="hostal";
-
 /**
  * Función para conectar, devuelve el objeto de la conexión
- * @return Object   Devuelve el objeto que contiene la conexión a la BD
+ * @return PDO          La instancia de la clase PDO que representa la conexión
+ * @throws PDOException Si se produce algún error que impide la conexión
  */
-function conectar() {
-$conexion = pg_connect("
-    user=$user,
-    password=$pass,
-    host=$host,
-    port=$port,
-    dbname=$bd
-") or die( "Error al conectar: ".pg_last_error() );
+function conectar(): PDO
+{
+    // Datos de Conexión
+    $controlador="pgsql"; // pgsql/mysql
+    $host="localhost";
+    $port="5432";
+    $dbname="hostal";
+    $user="hostal";
+    $pass="hostal";
 
-    return $conexion;
+    try {
+        return $conexion = new PDO("
+            $controlador:host=$host;
+            port=$port;
+            dbname=$bd;
+            user=$user;
+            password=$pass;
+            charset=UTF8;
+        ");
+    } catch (PDOException $e) {
+?>
+        <h1>Error catastrófico de base de datos: no se puede continuar</h1>
+        <p>
+            No ha sido posible la conexión con la base de datos
+        </p>
+<?php
+        throw $e;
+    }
 }
 
 /**
  * Función que desconecta la conexión previamente establecida con la BD
  */
-function desconectar() {
+function desconectar()
+{
     pg_close(conectar_pg());
 }
 ?>
