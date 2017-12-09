8ac36cf9b6d174f1511987d9bd1912720ae27b0e
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-09 23:40:30 +0100

    Etiquetas a minúsculas y limpiadas tabulaciones

--- metaetiquetas-generales.php
+++ metaetiquetas-generales.php
@@ -1,37 +1,37 @@
-		<META charset="utf-8" />
-		<META name="author" lang="es" content="Raúl Caro Pastorino, tecnico@fryntiz.es" />
-		<META name="organization" content="fryntiz" />
-		<META name='copyright' content='Nombre de la compañia/organización' />
-		<META name="keywords" content="administración, hotel, plantilla, web, fryntiz" /><!--Etiquetas-->
-		<META name="subject" content="Web para administrar Hotel" /><!--Descripción cortísima-->
-		<META name="description" content="" /><!--Breve resumen-->
-		<META name='language' content='ES' />
-		<META name="robots" content="index, follow" /> <!--Indica si es indexada (index|noindex) y si se siguen enlaces (follow|nofollow)-->
-		<META name="viewport" content="width=device-width, initial-scale=1.0" /><!--Web responsive, no zoom en smartphone. Si no es responsive css quitar-->
-		<META name="google-site-verification" content="????-->Código de verificación google WEBMASTER" /><!--Verifica mediante Search consle, webmaster tools de google-->
+<meta charset="utf-8" />
+<meta name="author" lang="es" content="Raúl Caro Pastorino, tecnico@fryntiz.es" />
+<meta name="organization" content="fryntiz" />
+<meta name='copyright' content='Nombre de la compañia/organización' />
+<meta name="keywords" content="administración, hotel, plantilla, web, fryntiz" /><!--Etiquetas-->
+<meta name="subject" content="Web para administrar Hotel" /><!--Descripción cortísima-->
+<meta name="description" content="" /><!--Breve resumen-->
+<meta name='language' content='ES' />
+<meta name="robots" content="index, follow" /> <!--Indica si es indexada (index|noindex) y si se siguen enlaces (follow|nofollow)-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" /><!--Web responsive, no zoom en smartphone. Si no es responsive css quitar-->
+<meta name="google-site-verification" content="????-->Código de verificación google WEBMASTER" /><!--Verifica mediante Search consle, webmaster tools de google-->
 
-		<!--Metaetiquetas para mostrar WEB en Facebook-->
-		<META property='fb:admins' content='????????' /> <!--ID de los usuarios admin del sitio-->
-		<META property='og:title' content='Titulo de la página' />
-		<META property='og:description' content='Descripción' />
-		<META property='og:image' content='http://www.dominioDelSitioWEB.es/img/logo.png' /><!--Miniatura que se mostrará junto al enlace-->
-		<META property="og:locale" content="es_ES" />
-		<META property="og:locale:alternate" content="es_LA" />
-		<META property='og:url' content='http://www.dominioDelSitioWEB.es/' /><!--Dominio del sitio-->
-		<META property='og:site_name' content='Nombre del sitio' />
-		<META property='og:type' content='website' /> <!--Normalmente website y article-->
+<!--Metaetiquetas para mostrar WEB en Facebook-->
+<meta property='fb:admins' content='????????' /> <!--ID de los usuarios admin del sitio-->
+<meta property='og:title' content='Titulo de la página' />
+<meta property='og:description' content='Descripción' />
+<meta property='og:image' content='http://www.dominioDelSitioWEB.es/img/logo.png' /><!--Miniatura que se mostrará junto al enlace-->
+<meta property="og:locale" content="es_ES" />
+<meta property="og:locale:alternate" content="es_LA" />
+<meta property='og:url' content='http://www.dominioDelSitioWEB.es/' /><!--Dominio del sitio-->
+<meta property='og:site_name' content='Nombre del sitio' />
+<meta property='og:type' content='website' /> <!--Normalmente website y article-->
 
-		<!--Metaetiquetas para mostrar WEB en Twitter-->
-		<META name="twitter:card" content="summary" /> <!--Define el tipo de tarjeta (summary|photo|player)-->
-		<META name="twitter:site" content="@Organization" /><!--@username de la pagina en twitter-->
-		<META name="twitter:creator" content="@Author" /> <!--@username del creador o autor del contenido-->
-		<META name='twitter:image' content='http://midominio.com/logo.png' />
-		<META name="twitter:title" content="Nombre de la publicación en twitter" />
-		<META name="twitter:description" content="Descripción de la página, máximo 200 caracteres" />
+<!--Metaetiquetas para mostrar WEB en Twitter-->
+<meta name="twitter:card" content="summary" /> <!--Define el tipo de tarjeta (summary|photo|player)-->
+<meta name="twitter:site" content="@Organization" /><!--@username de la pagina en twitter-->
+<meta name="twitter:creator" content="@Author" /> <!--@username del creador o autor del contenido-->
+<meta name='twitter:image' content='http://midominio.com/logo.png' />
+<meta name="twitter:title" content="Nombre de la publicación en twitter" />
+<meta name="twitter:description" content="Descripción de la página, máximo 200 caracteres" />
 
-		<!--Enlaces a favicon, CSS y JS-->
-		<LINK rel="shortcut icon" href="./img/favicon-32x32.png" />
-		<LINK rel="shortcut icon" href="./img/favicon-32x32.ico" />
-		<LINK rel="shortcut icon" href="./img/favicon-192x192.png" />
-		<LINK rel="stylesheet" href="./CSS/estilos.css" type="text/css" />
-		<SCRIPT src="./JS/scripts.js"></SCRIPT>
+<!--Enlaces a favicon, CSS y JS-->
+<link rel="shortcut icon" href="./img/favicon-32x32.png" />
+<link rel="shortcut icon" href="./img/favicon-32x32.ico" />
+<link rel="shortcut icon" href="./img/favicon-192x192.png" />
+<link rel="stylesheet" href="./CSS/estilos.css" type="text/css" />
+<script src="./JS/scripts.js"></script>
