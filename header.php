b0d6492e082e66b0fcf94fbeade5cf88b57762f8
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-09 23:19:39 +0100

    Etiquetas en minúsculas

--- header.php
+++ header.php
@@ -1,19 +1,21 @@
-<HEADER id="cajaTitulo">
-    <FIGURE>
-        <IMG src="./img/Logotipo.png" alt="Mi Logo" title="Logotipo del sitio web" longdesc="http://www.miweb.es/index.html#descripcionweb" />
-        <FIGCAPTION>
-            <H1 id="titulo">Panel de Administración Hotel</H1>
-            <P id="descripcionweb">Adminstra desde este panel clientes y habitaciones</P>
-        </FIGCAPTION>
-    </FIGURE>
-</HEADER>
+<header id="cajaTitulo">
+    <figure>
+        <img src="./img/Logotipo.png" alt="Mi Logo" title="Logotipo del sitio web" longdesc="http://www.miweb.es/index.html#descripcionweb" />
+        <figcaption>
+            <h1 id="titulo">Panel de Administración Hotel</h1>
+            <p id="descripcionweb">
+                Adminstra desde este panel clientes y habitaciones
+            </p>
+        </figcaption>
+    </figure>
+</header>
 
-<NAV id="cajaMenuPrincipal"><!--Menú principal bajo el título-->
-    <UL>
-        <LI><A href="./index.php" title="Inicio">Inicio</A></LI>
-        <LI><A href="./clientes.php" title="Clientes">Clientes</A></LI>
-        <LI><A href="./habitaciones.php" title="Habitaciones">Habitaciones</A></LI>
-        <LI><A href="./reservas.php" title="Reservas">Reservas</A></LI>
-        <LI><A href="./estadisticas.php" title="Estadísticas">Estadísticas</A></LI>
-    </UL>
-</NAV>
+<nav id="cajaMenuPrincipal"><!--Menú principal bajo el título-->
+    <ul>
+        <li><a href="./index.php" title="Inicio">Inicio</a></li>
+        <li><a href="./clientes.php" title="Clientes">Clientes</a></li>
+        <li><a href="./habitaciones.php" title="Habitaciones">Habitaciones</a></li>
+        <li><a href="./reservas.php" title="Reservas">Reservas</a></li>
+        <li><a href="./estadisticas.php" title="Estadísticas">Estadísticas</a></li>
+    </ul>
+</nav>
