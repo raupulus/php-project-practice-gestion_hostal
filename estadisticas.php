798d4093b488143a685e4fb8ddea7525e3875f9f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-10 14:56:37 +0100

    Reestructurado script

--- estadisticas.php
+++ estadisticas.php
@@ -10,57 +10,55 @@
 
     <BODY>
         <?php
-            include ('titulo.php');
-            include ('menu-principal.php');
-            echo    '<DIV id="cajaContenido">';
-            echo    '<DIV id="cajaEntradas">';
+            require ('funciones.php');
+            require ('header.php');
         ?>
 
-        <ARTICLE>
-            <HEADER>
-                <H2>Estadísticas</H2>
-                <P>Detalles sobre el desarrollo de las actividades</P>
-            </HEADER>
+        <div id="cajaContenido">
+            <div id="cajaEntradas">
+                <ARTICLE>
+                    <HEADER>
+                        <H2>Estadísticas</H2>
+                        <P>Detalles sobre el desarrollo de las actividades</P>
+                    </HEADER>
 
-            <SECTION>
-                <H3>Clientes</H3>
-                <P>Detalles sobre los clientes</P>
-                <UL>
-                    <LI>Clientes totales</LI>
-                    <LI>Añdadidos esta semana</LI>
-                    <LI>Añdadidos este mes</LI>
-                    <LI>Añadidos el último año</LI>
-                </UL>
-            </SECTION>
+                    <SECTION>
+                        <H3>Clientes</H3>
+                        <P>Detalles sobre los clientes</P>
+                        <UL>
+                            <LI>Clientes totales</LI>
+                            <LI>Añdadidos esta semana</LI>
+                            <LI>Añdadidos este mes</LI>
+                            <LI>Añadidos el último año</LI>
+                        </UL>
+                    </SECTION>
 
-            <SECTION>
-                <H3>Reservas</H3>
-                <P>Detalles sobre las Reservas</P>
-                <UL>
-                    <LI>Reservas totales</LI>
-                    <LI>Añdadidas esta semana</LI>
-                    <LI>Añdadidas este mes</LI>
-                    <LI>Añadidas el último año</LI>
-                </UL>
-            </SECTION>
+                    <SECTION>
+                        <H3>Reservas</H3>
+                        <P>Detalles sobre las Reservas</P>
+                        <UL>
+                            <LI>Reservas totales</LI>
+                            <LI>Añdadidas esta semana</LI>
+                            <LI>Añdadidas este mes</LI>
+                            <LI>Añadidas el último año</LI>
+                        </UL>
+                    </SECTION>
 
-            <SECTION>
-                <H3>Control de cuentes</H3>
-                <P>Detalles sobre los beneficios</P>
-                <UL>
-                    <LI>Ingresos totales</LI>
-                    <LI>Ingresos de esta semana</LI>
-                    <LI>Ingresos de este mes</LI>
-                    <LI>Ingresos del último año</LI>
-                </UL>
-            </SECTION>
-        </ARTICLE>
+                    <SECTION>
+                        <H3>Control de cuentas</H3>
+                        <P>Detalles sobre los beneficios</P>
+                        <UL>
+                            <LI>Ingresos totales</LI>
+                            <LI>Ingresos de esta semana</LI>
+                            <LI>Ingresos de este mes</LI>
+                            <LI>Ingresos del último año</LI>
+                        </UL>
+                    </SECTION>
+                </ARTICLE>
+            </div>
 
-        <?php
-            echo '</DIV>';
-            include ('aside.php');
-            echo    '</DIV>';
-            include ('footer.php');
-        ?>
-    </BODY>
-</HTML>
+        <?php require ('aside.php'); ?>
+        </div>
+        <?php require ('footer.php'); ?>
+    </body>
+</html>
