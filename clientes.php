dedb996921f39a8aff897d99350369432b4ce27f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-10 01:55:26 +0100

    Etiquetas transformadas a minúsculas

--- clientes.php
+++ clientes.php
@@ -1,195 +1,195 @@
 <!DOCTYPE html>
-<HTML lang="es">
-    <HEAD>
-        <TITLE>Administración Hotel</TITLE>
+<html lang="es">
+    <head>
+        <title>Administración Hotel</title>
         <?php //Metaetiquetas globales importadas
             require ('metaetiquetas-generales.php');
         ?>
-        <META name='revised' content='Tuesday, February 13th, 2017, 13:22 pm' /><!--Última revisión del sitio-->
-    </HEAD>
+        <meta name='revised' content='Tuesday, February 13th, 2017, 13:22 pm' /><!--Última revisión del sitio-->
+    </head>
 
-    <BODY>
+    <body>
         <?php
             require ('funciones.php');
             require ('header.php');
         ?>
 
-        <DIV id="cajaContenido">
-            <DIV id="cajaEntradas">
-                <ARTICLE>
-                    <HEADER>
-                        <H2>CLIENTES</H2>
+        <div id="cajaContenido">
+            <div id="cajaEntradas">
+                <article>
+                    <header>
+                        <h2>CLIENTES</h2>
                         <P>Detalles de clientes registrados</P>
-                        <TIME datetime="2017-02-14T01:20:00+01:00"><SMALL>Último cliente registrado 14-02-2017 a las 01:20</SMALL></TIME>
-                    </HEADER>
+                        <time datetime="2017-02-14T01:20:00+01:00"><small>Último cliente registrado 14-02-2017 a las 01:20</small></time>
+                    </header>
 
-                    <DIV class="agregar" onClick="location.href = 'form-clientes.php'">
+                    <div class="agregar" onClick="location.href = 'form-clientes.php'">
                         +Nuevo Cliente
-                    </DIV>
+                    </div>
 
-                    <DIV class="agregar mostrarBuscar" onClick="document.getElementById('buscar').style.display = 'block';">
+                    <div class="agregar mostrarBuscar" onClick="document.getElementById('buscar').style.display = 'block';">
                         Buscar Cliente
-                    </DIV>
+                    </div>
 
-                    <DIV id="buscar">
+                    <div id="buscar">
                         <FORM>
-                            <INPUT type="button" value="Buscar" onClick="document.getElementById('busqueda').style.display = 'block';"/>
-                            Nombre <INPUT type="search" />
-                            Apellidos <INPUT type="search" />
+                            <input type="button" value="Buscar" onClick="document.getElementById('busqueda').style.display = 'block';"/>
+                            Nombre <input type="search" />
+                            Apellidos <input type="search" />
                             <BR /><BR />
-                            DNI <INPUT type="search" />
-                            Teléfono <INPUT type="search" />
+                            DNI <input type="search" />
+                            Teléfono <input type="search" />
                         </FORM>
-                    </DIV>
+                    </div>
 
-                    <SECTION id="busqueda"><!--No se muestra hasta que se pulsa buscar-->
-                        <H3>Resultado de la búsqueda</H3>
+                    <section id="busqueda"><!--No se muestra hasta que se pulsa buscar-->
+                        <h3>Resultado de la búsqueda</h3>
                         <P>Las coincidencias con la búsqueda son:</P>
-                        <TABLE>
-                            <TR>
-                                <TH>Nombre</TH>
-                                <TH>Apellidos</TH>
-                                <TH>Teléfono</TH>
-                                <TH>Provincia</TH>
-                                <TH>Ciudad</TH>
-                                <TH>Tiene Reservas</TH>
-                            </TR>
-                        </TABLE>
-                    </SECTION>
+                        <table>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Teléfono</th>
+                                <th>Provincia</th>
+                                <th>Ciudad</th>
+                                <th>Tiene Reservas</th>
+                            </tr>
+                        </table>
+                    </section>
 
-                    <SECTION>
-                        <H3>Con reserva en este momento</H3>
+                    <section>
+                        <h3>Con reserva en este momento</h3>
                         <P>Clientes que tienen reserva</P>
                         <table>
-                            <TR>
-                                <TH>Nombre</TH>
-                                <TH>Apellidos</TH>
-                                <TH>Teléfono</TH>
-                                <TH>Provincia</TH>
-                                <TH>Ciudad</TH>
-                                <TH>Nº Habitación</TH>
-                                <TH>Días</TH>
-                                <TH>Precio</TH>
-                            </TR>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Teléfono</th>
+                                <th>Provincia</th>
+                                <th>Ciudad</th>
+                                <th>Nº Habitación</th>
+                                <th>Días</th>
+                                <th>Precio</th>
+                            </tr>
 
                             <?php
                                 $clientes = consulta_clientes_con_reserva();
                                 foreach ($clientes as $key => $value):
                             ?>
-                                    <TR>
-                                        <TD><?= $value['nombre'] ?></TD>
-                                        <TD><?= $value['apellidos'] ?></TD>
-                                        <TD><?= $value['telefono'] ?></TD>
-                                        <TD><?= $value['provincia'] ?></TD>
-                                        <TD><?= $value['ciudad'] ?></TD>
-                                        <TD><?= $value['numeros'] ?></TD>
-                                        <TD><?= $value['dias_reservados'] ?></TD>
-                                        <TD><?= $value['precio'] ?></TD>
-                                    </TR>
+                                    <tr>
+                                        <td><?= $value['nombre'] ?></td>
+                                        <td><?= $value['apellidos'] ?></td>
+                                        <td><?= $value['telefono'] ?></td>
+                                        <td><?= $value['provincia'] ?></td>
+                                        <td><?= $value['ciudad'] ?></td>
+                                        <td><?= $value['numeros'] ?></td>
+                                        <td><?= $value['dias_reservados'] ?></td>
+                                        <td><?= $value['precio'] ?></td>
+                                    </tr>
                             <?php
                                 endforeach;
                             ?>
                         </table
-                    </SECTION>
+                    </section>
 
-                    <SECTION>
-                        <H3>Sin reserva en este momento</H3>
+                    <section>
+                        <h3>Sin reserva en este momento</h3>
                         <P>Clientes que tienen no tienen reserva</P>
-                        <TABLE>
-                            <TR>
-                                <TH>Nombre</TH>
-                                <TH>Apellidos</TH>
-                                <TH>Teléfono</TH>
-                                <TH>Provincia</TH>
-                                <TH>Ciudad</TH>
-                            </TR>
+                        <table>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Teléfono</th>
+                                <th>Provincia</th>
+                                <th>Ciudad</th>
+                            </tr>
 
                             <!--Temporal para diseño, esta parte se llena con la BD-->
-                            <TR>
-                                <TD>Pepe</TD>
-                                <TD>Mero Calistro</TD>
-                                <TD>612123123</TD>
-                                <TD>Cádiz</TD>
-                                <TD>Chipiona</TD>
-                            </TR>
-                            <TR>
-                                <TD>Ana</TD>
-                                <TD>Corneta Quemada</TD>
-                                <TD>6987987</TD>
-                                <TD>Sevilla</TD>
-                                <TD>Dos Hermanas</TD>
-                            </TR>
+                            <tr>
+                                <td>Pepe</td>
+                                <td>Mero Calistro</td>
+                                <td>612123123</td>
+                                <td>Cádiz</td>
+                                <td>Chipiona</td>
+                            </tr>
+                            <tr>
+                                <td>Ana</td>
+                                <td>Corneta Quemada</td>
+                                <td>6987987</td>
+                                <td>Sevilla</td>
+                                <td>Dos Hermanas</td>
+                            </tr>
                             <!--Fin de la parte temporal-->
 
                             <?php
 
                             ?>
-                        </TABLE>
-                    </SECTION>
+                        </table>
+                    </section>
 
-                    <SECTION>
-                        <H3>Clientes que nunca han reservados</H3>
+                    <section>
+                        <h3>Clientes que nunca han reservados</h3>
                         <P>Clientes que no han creado nuna una reserva</P>
-                        <TABLE>
-                            <TR>
-                                <TH>Nombre</TH>
-                                <TH>Apellidos</TH>
-                                <TH>Teléfono</TH>
-                                <TH>Provincia</TH>
-                                <TH>Ciudad</TH>
-                            </TR>
+                        <table>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Teléfono</th>
+                                <th>Provincia</th>
+                                <th>Ciudad</th>
+                            </tr>
 
                             <!--Temporal para diseño, esta parte se llena con la BD-->
-                            <TR>
-                                <TD>Pepe</TD>
-                                <TD>Mero Calistro</TD>
-                                <TD>612123123</TD>
-                                <TD>Cádiz</TD>
-                                <TD>Chipiona</TD>
-                            </TR>
-                            <TR>
-                                <TD>Ana</TD>
-                                <TD>Corneta Quemada</TD>
-                                <TD>6987987</TD>
-                                <TD>Sevilla</TD>
-                                <TD>Dos Hermanas</TD>
-                            </TR>
+                            <tr>
+                                <td>Pepe</td>
+                                <td>Mero Calistro</td>
+                                <td>612123123</td>
+                                <td>Cádiz</td>
+                                <td>Chipiona</td>
+                            </tr>
+                            <tr>
+                                <td>Ana</td>
+                                <td>Corneta Quemada</td>
+                                <td>6987987</td>
+                                <td>Sevilla</td>
+                                <td>Dos Hermanas</td>
+                            </tr>
                             <!--Fin de la parte temporal-->
-                        </TABLE>
-                    </SECTION>
+                        </table>
+                    </section>
 
-                    <SECTION>
-                        <H3>Todos los clientes</H3>
+                    <section>
+                        <h3>Todos los clientes</h3>
                         <P>Tablas con todos los clientes existentes</P>
-                        <TABLE>
-                            <TR>
-                                <TH>Nombre</TH>
-                                <TH>Apellidos</TH>
-                                <TH>Teléfono</TH>
-                                <TH>Provincia</TH>
-                                <TH>Ciudad</TH>
-                            </TR>
+                        <table>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellidos</th>
+                                <th>Teléfono</th>
+                                <th>Provincia</th>
+                                <th>Ciudad</th>
+                            </tr>
                             <?php
                                 $clientes = consulta_clientes_todos();
                                 foreach ($clientes as $key => $value):
                                 ?>
-                                    <TR>
-                                        <TD><?= $value['nombre'] ?></TD>
-                                        <TD><?= $value['apellidos'] ?></TD>
-                                        <TD><?= $value['telefono'] ?></TD>
-                                        <TD><?= $value['provincia'] ?></TD>
-                                        <TD><?= $value['ciudad'] ?></TD>
-                                    </TR>
+                                    <tr>
+                                        <td><?= $value['nombre'] ?></td>
+                                        <td><?= $value['apellidos'] ?></td>
+                                        <td><?= $value['telefono'] ?></td>
+                                        <td><?= $value['provincia'] ?></td>
+                                        <td><?= $value['ciudad'] ?></td>
+                                    </tr>
                                 <?php
                                 endforeach;
                             ?>
-                        </TABLE>
-                    </SECTION>
-                </ARTICLE>
-            </DIV>
+                        </table>
+                    </section>
+                </article>
+            </div>
 
         <?php require ('aside.php'); ?>
-        </DIV>
+        </div>
         <?php require ('footer.php'); ?>
-    </BODY>
-</HTML>
+    </body>
+</html>
